1c44c6dc287e1b8be57b684db51bdcf7fb8b9515
wesleypribeiro <wesley_paulo00@hotmail.com>
2020-10-23 17:24:49 -0300

    Adicionado o exercicio 3

---
+++ exercises/introducao-a-javascript-e-logica-de-programacao_1/exercicio3.js
@@ -0,0 +1,11 @@
+var a = 5;
+var b = 10;
+var c = 1;
+
+if (a > b && a > c) {
+    console.log("A variavel a é maior de todas");
+} else if (b > a && b > c) {
+    console.log("A variavel b é maior de todas");
+} else if (c > a && c > b) {
+    console.log("A variavel c é a maior de todas");
+}
