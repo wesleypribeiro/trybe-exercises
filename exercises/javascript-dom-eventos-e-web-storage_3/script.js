67c8ddb7b33082dcc3441117fa5b59bf69f9d508
wesleypribeiro <wesley_paulo00@hotmail.com>
2020-11-04 18:54:41 -0300

    Adicionado o exercicio 1

---
+++ exercises/javascript-dom-eventos-e-web-storage_3/script.js
@@ -0,0 +1,37 @@
+function createDaysOfTheWeek() {
+    const weekDays = ['Domingo', 'Segunda', 'Terça', 'Quarta', 'Quinta', 'Sexta', 'Sábado']
+    const weekDaysList = document.querySelector('.week-days');
+  
+    for (let index = 0; index < weekDays.length; index += 1) {
+      const days = weekDays[index];
+      const dayListItem = document.createElement('li');
+      dayListItem.innerHTML = days;
+  
+      weekDaysList.appendChild(dayListItem);
+    };
+  };
+  
+  createDaysOfTheWeek();
+  
+  // Escreva seu código abaixo.
+
+
+  // EXERCICIO 1
+  function createDaysOfTheMonth(){
+    const dezDaysList = [29, 30, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31];
+
+    for (let index = 0; index < dezDaysList.length; index += 1) {
+        let day = document.createElement('li');
+        day.className = "day";
+        day.innerText = dezDaysList[index];
+
+        if (day.innerText == '24' || day.innerText == '25' || day.innerText == '31' ) { day.className = day.className + ' holiday'; }
+        if (day.innerText == '4' || day.innerText == '11' || day.innerText == '18' || day.innerText == '25') { day.className = day.className + ' friday'; }
+
+        document.querySelector('#days').appendChild(day);
+    }
+  }
+
+  createDaysOfTheMonth();
+
+  
