7c56b2f8ab0dfe832a05703bded0b4e811aa3d10
wesleypribeiro <wesley_paulo00@hotmail.com>
2020-11-24 18:33:47 -0300

    Adicionado P01E02

--- exercises/introducao-a-javascript-es6-e-testes-unitarios_2/tempCodeRunnerFile.js
+++ exercises/introducao-a-javascript-es6-e-testes-unitarios_2/tempCodeRunnerFile.js
@@ -35,8 +35,20 @@ const order = {
 };
 
 const customerInfo = (order) => {
+  // Adicione abaixo as informações necessárias.
   console.log(`Olá ${order.order.delivery.deliveryPerson}, entrega para: ${order.name}, Telefone: ${order.phoneNumber}, ${order.address.street}, Nº: ${order.address.number}, AP: ${order.address.apartment}`);
 
 }
 
-customerInfo(order);
+//customerInfo(order);
+
+const orderModifier = (order) => {
+  // Adicione abaixo as informações necessárias.
+  order.name = 'Luiz Silva';
+  order.order.pizza.margherita.name = 'muzzarella';
+  order.order.pizza.pepperoni.name = 'calabresa';
+  order.payment.total = 50;
+  console.log(`Olá ${order.name}, o total do seu pedido de ${order.order.pizza.margherita.name}, ${order.order.pizza.pepperoni.name} e ${order.order.drinks.coke.type} é R$ ${order.payment.total},00.`);
+}
+
+orderModifier(order);
